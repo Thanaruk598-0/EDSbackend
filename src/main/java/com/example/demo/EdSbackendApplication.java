83b16eb81a7b5af656567d9eacb64580185c16e9
Thanaruk598-0 <Thanaruk.su@kkumail.com>
2025-09-20 15:39:07 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/EdSbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdSbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdSbackendApplication.class, args);
+	}
+
+}
